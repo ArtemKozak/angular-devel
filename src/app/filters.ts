2630f52cfd35e0e982e2c21f89639fddb2222b1b
Druid72 <timon4k37@gmail.com>
2019-11-20 01:16:40 +0200

    3 files modified, 1 added

---
+++ src/app/filters.ts
@@ -0,0 +1,14 @@
+export const filters = [
+  {
+    name: "Производитель",
+    filt: ["Apple", "Samsung", "Xiaomi"],
+  },
+  {
+    name: "Цена",
+    filt: [0, 200, 500, 800],
+  },
+  {
+    name: "Экран",
+    filt: [0, 5, 6, 7],
+  },
+]
--- src/app/product-filter/product-filter.component.ts
+++ src/app/product-filter/product-filter.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import { filters } from '../filters';
+
 @Component({
   selector: 'app-product-filter',
   templateUrl: './product-filter.component.html',
   styleUrls: ['./product-filter.component.css']
 })
 export class ProductFilterComponent implements OnInit {
-
+  filters = filters;
+  
   constructor() { }
 
   ngOnInit() {
